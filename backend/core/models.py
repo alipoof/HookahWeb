eba5f480e259120a396b07d61db0b44980c07c4b
SvyatS <prisenko2002@gmail.com>
2021-04-20 13:38:34 +0700

    init backend

---
+++ backend/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class ExtendedUser(models.Model):
+    pass
+
+
+class Table(models.Model):
+    pass
+
+
+class Reserve(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    pass
